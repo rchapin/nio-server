dd056ddf985c8440079243f167cc1d0ba0354303
Ryan Chapin <rchapin@users.noreply.github.com>
2015-01-04 17:37:13 -0500

    Adding basic maven project.

---
+++ src/main/java/com/ryanchapin/NioServer/NioServer.java
@@ -0,0 +1,13 @@
+package com.ryanchapin.NioServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class NioServer
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
