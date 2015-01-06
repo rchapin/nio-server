9240b3ecd3aba0eac7ad40445e162c068320ef92
Ryan Chapin <rchapin@users.noreply.github.com>
2015-01-05 22:50:41 -0500

    Removed the NioServer interface and renamed the implementation class 'NioServer'.
    . renamed:
      src/main/java/com/ryanchapin/nio_server/DefaultNioServer.java
        to
      src/main/java/com/ryanchapin/nio_server/NioServer.java
      . Basically, got rid of the interface, since I don't need it
        right now.

--- src/main/java/com/ryanchapin/nio_server/NioServer.java
+++ src/main/java/com/ryanchapin/nio_server/NioServer.java
@@ -1,23 +1,17 @@
 package com.ryanchapin.nio_server;
 
 /**
- * @since    2015-01-04
+ * 
+ * @since 2015-01-04
+ *
  */
-public interface NioServer extends Runnable, Comparable<NioServer> {
+public class NioServer implements Runnable, Comparable<NioServer> {
 
-	/**
-	 * Requires invocation before attempting to run.  Running after initial
-	 * initialization and after invoking run will shutdown the server, closing
-	 * all existing client connections and will re-initialize the server.
-	 */
-	public void init();
+	private int portNumber;
 	
-	/**
-	 * Returns a boolean indicating whether or not the instance is initialized.
-	 * 
-	 * @return  boolean value indicating whether or not the instance is initialized.  
-	 */
-	public boolean isInitialized();
+	// ------------------------------------------------------------------------
+	// Accessor/Mutators:
+	//
 	
 	/**
 	 * Will return the port number on which the NioServer instance is to listen
@@ -25,12 +19,65 @@ public interface NioServer extends Runnable, Comparable<NioServer> {
 	 * .
 	 * @return  the port number on which the server is configured to listen.
 	 */
-	public int getPortNumber();
+	public int getPortNumber() {
+		return portNumber;
+	}	
+
+	/**
+	 * Returns a boolean indicating whether or not the instance is initialized.
+	 * 
+	 * @return  boolean value indicating whether or not the instance is initialized.  
+	 */
+	public boolean isInitialized() {
+		return false;
+	}
 	
-	public int compareTo(NioServer o);
+	// ------------------------------------------------------------------------
+	// Constructor
+	//
 	
-	public boolean equals(NioServer other);
+	/**
+	 * 
+	 * @param portNumber
+	 *        The port number to which the NioServer instance will bind and
+	 *        listen.
+	 */
+	public NioServer(int portNumber) {
+		this.portNumber = portNumber;
+	}
+
+
+
+	// ------------------------------------------------------------------------
+	// Member Methods:
+	//
 	
-	public int hashCode();
+	/**
+	 * Requires invocation before attempting to run.  Running after initial
+	 * initialization and after invoking run will shutdown the server, closing
+	 * all existing client connections and will re-initialize the server.
+	 */
+	public void init() {
+	}
 	
+	
+	public void run() {
+
+	}
+
+	public int compareTo(NioServer o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean equals(NioServer other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO
+		return 0;
+	}
 }
